3c73e5ab190d1aad7dd24dcbe339b247498ac6ca
Krishnendu Saha <krishnendu5588@gmail.com>
2026-08-10 19:52:29 +0530

    Time: 43 ms (6.61%), Space: 14.1 MB (15.02%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        i=0
+        j=0
+        count=0
+        g.sort()
+        s.sort()
+        while i<len(g) and j<len(s):
+            if g[i]<=s[j]:
+                i=i+1
+                j=j+1
+                count=count+1
+            elif g[i]>s[j]:
+                j=j+1
+        return count
+
+        
