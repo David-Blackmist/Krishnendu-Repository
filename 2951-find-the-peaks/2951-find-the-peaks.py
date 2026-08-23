b7948040fa6f8666fc986b21e297da3bf20b64ed
Krishnendu Saha <krishnendu5588@gmail.com>
2026-08-23 11:07:59 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.55%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findPeaks(self, mountain):
+        arr=[]
+        for i in range(1,len(mountain)-1):
+            if mountain[i]>mountain[i-1] and mountain[i]>mountain[i+1]:
+                arr.append(i)
+        return arr
+                
+        
